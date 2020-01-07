01da63a2a029f40181fb887db92b8db51aeb2e5a
Ryan-Wisniewski <Ryanwis98@yahoo.com>
2020-01-06 22:08:18 -0500

    name_shuffle tests pass

--- EdabitChalenges.js
+++ EdabitChalenges.js
@@ -15,32 +15,44 @@
 # # simon_says([1, 2, 3, 4, 5], [0, 1, 2, 3, 4])
 # simon_says([1, 2, 3, 4, 5], [5, 5, 1, 2, 3])
 
-def can_alternate(s):
-	#takes a binary string and sees if it can split the 0's and 1's evenly
-    counter_x = 0
-    counter_y = 0
-    for x in s:
-        if x == '0':
-            counter_x += 1
-            # print(counter_x)
-        elif x == '1':
-            counter_y += 1
-            # print(counter_y)
+# def can_alternate(s):
+# 	#takes a binary string and sees if it can split the 0's and 1's evenly
+#     counter_x = 0
+#     counter_y = 0
+#     for x in s:
+#         if x == '0':
+#             counter_x += 1
+#             # print(counter_x)
+#         elif x == '1':
+#             counter_y += 1
+#             # print(counter_y)
         
-    # print(counter_x, counter_y)
-    if counter_x >= counter_y - 1 and counter_x + 1 <= counter_y:
-        print('t1', counter_x, counter_y)
-        return True
-    elif counter_x + 1 >= counter_y and counter_x - 1 <= counter_y:
-        print('t2', counter_x, counter_y)
-        return True
-        # print(x)
-    else: 
-        print('f', counter_x, counter_y)
-        return False
+#     # print(counter_x, counter_y)
+#     if counter_x >= counter_y - 1 and counter_x + 1 <= counter_y:
+#         # print('t1', counter_x, counter_y)
+#         return True
+#     elif counter_x + 1 >= counter_y and counter_x - 1 <= counter_y:
+#         # print('t2', counter_x, counter_y)
+#         return True
+#         # print(x)
+#     else: 
+#         # print('f', counter_x, counter_y)
+#         return False
 
-can_alternate("000101")
-can_alternate("010101")
-can_alternate("1100101")
-can_alternate("101")
-can_alternate("000")
+# # can_alternate("000101")
+# # can_alternate("010101")
+# # can_alternate("1100101")
+# # can_alternate("101")
+# # can_alternate("000")
+
+def name_shuffle(txt):
+	#replaces first with last name
+    x = txt.split(' ')
+    newStr = f'{x[1]} {x[0]}'
+    print(type(txt))
+    # print(newStr)
+
+name_shuffle("Donald Trump")
+name_shuffle("Rosie O'Donnell")
+name_shuffle("Seymour Butts")
+
